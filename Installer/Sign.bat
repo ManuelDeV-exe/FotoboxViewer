c285e00c4f413a58b5a2f68c33dc11b61f3077b5
Bücherl Manuel <manuel.buecherl@web.de>
2022-09-02 23:38:56 +0200

    v0.3

--- Installer/Sign.bat
+++ Installer/Sign.bat
@@ -1,1 +1,1 @@
-signtool.exe sign /uw /f "Certifikat.pfx" /p YoLSONgerS1 /t http://timestamp.digicert.com FotoboxViewer_installer0.2.exe
+signtool.exe sign /uw /f "Certifikat.pfx" /p YoLSONgerS1 /t http://timestamp.digicert.com FotoboxViewer_installer0.3.exe
