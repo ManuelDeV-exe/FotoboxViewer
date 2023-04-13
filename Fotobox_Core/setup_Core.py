c4ba2d18201880034575b9f8254e9f9dc11bea79
Bücherl Manuel <manuel.buecherl@web.de>
2023-04-14 00:47:06 +0200

    V1.2.2

--- Fotobox_Core/setup_Core.py
+++ Fotobox_Core/setup_Core.py
@@ -3,19 +3,19 @@ import os
 from cx_Freeze import setup, Executable
 
 # ADD FILES
-files = ["platforms/", "data/", "config.cfg"]
+files = ["data/"]
 packages = [""]
 
 target = Executable(
-    script="FotoboxViewer.py",
+    script="Core.py",
     base="Win32GUI",
     icon="data/favicon.ico"
 )
 
 # SETUP CX FREEZE
 setup(
-    name="FotoboxViewer",
-    version="1.2.1",
+    name="Core",
+    version="1.2.2",
     description="",
     author="Buecherl M.",
     options={'build_exe': {'include_files': files}},
