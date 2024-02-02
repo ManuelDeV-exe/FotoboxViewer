4e347effb5b5c33b305d10b34548f73fac4cf4fa
ManuelDeV-exe <manuel.buecherl@web.de>
2024-02-02 01:13:25 +0100

    V1.5.0

--- ui_Loading_screen.py
+++ ui_Loading_screen.py
@@ -1,3 +1,3 @@
 Error: File 'Loading_screen.ui' is not valid
 
-while executing 'C:\Users\Detag\AppData\Local\Programs\Python\Python311\Lib\site-packages\PySide6\uic -g python Loading_screen.ui'
+while executing 'C:\Users\b_m21\AppData\Local\Programs\Python\Python311\Lib\site-packages\PySide6\uic -g python Loading_screen.ui'
